e52e7f6adfe1df525059e403a34c5c9435b36977
Anand Thakker <vestibule@anandthakker.net>
2015-11-14 13:57:23 -0500

    Remove boundary simplification

--- dbscripts/sql/setup-boundaries.sql
+++ dbscripts/sql/setup-boundaries.sql
@@ -1,14 +1,14 @@
 BEGIN;
 
 --- Generate district key
-ALTER TABLE districts_boundaries ADD COLUMN district_key varchar(50);
-UPDATE districts_boundaries SET
+ALTER TABLE districts_boundaries_import ADD COLUMN district_key varchar(50);
+UPDATE districts_boundaries_import SET
   district_key = lower(replace(state_ut,' ','-')) || '-' || lower(replace(district,' ', '-'));
 
 --- Dissolve duplicate districts
 --- NOTE: we average the population data because it's duplicated in the shpaefile.
-drop table if exists districts_boundaries_merged;
-create table districts_boundaries_merged as
+drop table if exists districts_boundaries;
+create table districts_boundaries as
 select
   district_key,
   lower(replace(state_ut, ' ', '-')) as state_key,
@@ -17,28 +17,25 @@ select
   avg(tot_lit) as tot_lit,
   ST_Union(geom) as geom,
   ST_Extent(geom) as bbox
-from districts_boundaries
+from districts_boundaries_import
 group by state_key, district_key;
-
-drop table districts_boundaries;
-alter table districts_boundaries_merged rename to districts_boundaries;
 
 --- Simplify boundaries using a topology
 -- Create a topology
-select topology.droptopogeometrycolumn('public', 'districts_boundaries', 'topogeom');
-select topology.DropTopology('districts_topo');
-select topology.CreateTopology('districts_topo', find_srid('public', 'districts_boundaries', 'geom'));
--- Add a layer
-select topology.addtopogeometrycolumn('districts_topo', 'public', 'districts_boundaries', 'topogeom', 'MULTIPOLYGON');
--- Populate the layer and the topology: this step takes a while.
-UPDATE districts_boundaries SET topogeom = topology.toTopoGeom(geom, 'districts_topo', 1);
-
+--- select topology.droptopogeometrycolumn('public', 'districts_boundaries', 'topogeom');
+--- select topology.DropTopology('districts_topo');
+--- select topology.CreateTopology('districts_topo', find_srid('public', 'districts_boundaries', 'geom'));
+--- -- Add a layer
+--- select topology.addtopogeometrycolumn('districts_topo', 'public', 'districts_boundaries', 'topogeom', 'MULTIPOLYGON');
+--- -- Populate the layer and the topology: this step takes a while.
+--- UPDATE districts_boundaries SET topogeom = topology.toTopoGeom(geom, 'districts_topo', 1);
+--- 
 --- Simplify (see init-postgis.sql)
-SELECT SimplifyEdgeGeom('districts_topo', edge_id, 0.01) FROM districts_topo.edge;
+--- SELECT SimplifyEdgeGeom('districts_topo', edge_id, 0.01) FROM districts_topo.edge;
 -- Convert the TopoGeometries to Geometries for visualization
-ALTER TABLE districts_boundaries DROP COLUMN geom_simplified;
-ALTER TABLE districts_boundaries ADD geom_simplified GEOMETRY;
-UPDATE districts_boundaries SET geom_simplified = topogeom::geometry;
+-- ALTER TABLE districts_boundaries DROP COLUMN geom_simplified;
+-- ALTER TABLE districts_boundaries ADD geom_simplified GEOMETRY;
+-- UPDATE districts_boundaries SET geom_simplified = topogeom::geometry;
 
 --- Dissolve districts into states
 
