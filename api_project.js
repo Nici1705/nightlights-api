b63307dbde34cc22167dd548f1c895e81aa29121
Travis-CI <travis@somewhere.com>
2016-03-20 17:03:31 +0000

    CI deploy to gh-pages

---
+++ api_project.js
@@ -0,0 +1,18 @@
+define({
+  "name": "india-lights-api",
+  "version": "0.0.1",
+  "description": "An API to India Lights Data",
+  "title": "India Lights API documentation",
+  "url": "http://api.nightlights.io",
+  "header": {
+    "content": "<p>The India Lights API shows light output at night for 20 years, from 1993 to 2013, for 600,000 villages across India. The Defense Meteorological Satellite Program (DMSP), run by the U.S. Department of Defense, has taken pictures of the Earth every night for 20 years. Researchers at the University of Michigan used the DMSP images to extract the data and provide it in tabular form. The India Lights API provides the data at convenient endpoints that allows you to look at specific time intervals and administration levels.</p>\n<p>Browse the data at <a href=\"http://nightlights.io\">nightlights.io</a> or try the API at <a href=\"http://api.nightlights.io\">http://api.nightlights.io</a>. You can also run your own instance! Get the code on <a href=\"https://github.com/wbg-bigdata/nightlights-api\">Github</a></p>\n"
+  },
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-03-20T17:03:20.468Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.2"
+  }
+});
